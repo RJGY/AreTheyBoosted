f5b18ff95d73b4864e09437a19823665f69530ea
RJGY <rjgy2yu@gmail.com>
2022-02-16 00:07:54 +1100

    added more cases

--- AreTheyBoosted/tests.py
+++ AreTheyBoosted/tests.py
@@ -1,8 +1,14 @@
 import unittest
 from api import get_match
 
-class TestAPIMethods(unittest.TestCase):
-    def test_get_match(self):
+class TestGetMatchAPIMethods(unittest.TestCase):
+    def get_match_no_params(self):
+        self.assertEqual(get_match.get_match_ids(), None)
+
+    def get_match_only_region_param(self):
+        self.assertEqual(get_match.get_match_ids(), None)
+
+    def get_match_only_name_param(self):
         self.assertEqual(get_match.get_match_ids(), None)
 
 
