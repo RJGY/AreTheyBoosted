d3d1b077d56dff1f9ec67931eef9d20c111d8c83
RJGY <rjgy2yu@gmail.com>
2022-02-17 21:43:10 +1100

    added tests which work

--- AreTheyBoosted/tests.py
+++ AreTheyBoosted/tests.py
@@ -1,15 +1,25 @@
 import unittest
-from api import get_match
+import sys
+import os
+sys.path.append(os.getcwd() + '\\api')
+import api
 
-class TestGetMatchAPIMethods(unittest.TestCase):
-    def get_match_no_params(self):
-        self.assertEqual(get_match.get_match_ids(), None)
 
-    def get_match_only_region_param(self):
-        self.assertEqual(get_match.get_match_ids(), None)
+class TestMatchAPIMethods(unittest.TestCase):
+    def test_get_match_no_params(self):
+        self.assertEqual(api.match.get_match_ids(), None)
 
-    def get_match_only_name_param(self):
-        self.assertEqual(get_match.get_match_ids(), None)
+    def test_get_match_only_region_param(self):
+        self.assertEqual(api.match.get_match_ids(None, "Oceania"), None)
+
+    def test_get_match_only_name_param(self):
+        self.assertEqual(api.match.get_match_ids("Reese", None), None)
+
+    def test_get_match_only_incorrect_name_param(self):
+        self.assertEqual(api.match.get_match_ids("Re", "Oceania"), None)
+
+    def test_get_match_only_incorrect_region_param(self):
+        self.assertEqual(api.match.get_match_ids("Reese", "Oc"), None)
 
 
 if __name__ == '__main__':
